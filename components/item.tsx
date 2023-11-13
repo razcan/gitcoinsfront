1d1d5af0963414d1cc6cf3633e70859184537764
Razvan <razvan.mustata@gmail.com>
2023-11-13 17:07:26 +0200

    modal template

--- components/item.tsx
+++ components/item.tsx
@@ -15,6 +15,10 @@ import { Image } from 'primereact/image';
 import { Rating } from 'primereact/rating';
 import { Tag } from 'primereact/tag';
 import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+import { Galleria } from 'primereact/galleria';
+
+import { Divider } from 'primereact/divider';
+
 
 export default function Item() {
 
@@ -22,6 +26,18 @@ export default function Item() {
     const prefix_api = 'http://localhost:3000/coins/download/';
     const [products, setProducts] = useState([]);
     const [layout, setLayout] = useState('grid');
+    const [visible, setVisible] = useState(false);
+    const [selected_product, setSelected_product] = useState([]);
+    const [images, setImages] = useState(null);
+    const [ordered_qtty, setOrdered_qtty] = useState(1);
+
+
+    const setProductVisible = (product: any, bol: boolean) => {
+        setSelected_product(product);
+        setVisible(true);
+        console.log(product);
+    }
+
 
     const fetchCoinData = () => {
         fetch("http://localhost:3000/coins")
@@ -31,24 +47,13 @@ export default function Item() {
             .then(coins => {
                 setProducts(coins)
                 // setProducts(coins)
-              // console.log(coins)
+                // console.log(coins)
             })
     }
 
     useEffect(() => {
         fetchCoinData()
     }, [])
-
-
-    const [visible, setVisible] = useState(false);
-    const [modal_selected_item, setModal_selected_item] = useState([]);
-    const [order_qtty, setOrder_qtty] = useState(1);
-   
-
-    const chooseModalItem = (visible: string) => {
-        setVisible(true);
-        setModal_selected_item(visible);
-    };
 
 
 
@@ -81,7 +86,7 @@ export default function Item() {
 
     const gridItem = (product) => {
         return (
-            <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
+            <div className="col-6 sm:col-6 lg:col-6 xl:col-2 p-2">
                 <div className="p-4 border-1 surface-border surface-card border-round">
                     <div className="flex flex-wrap align-items-center justify-content-between gap-2">
                         <div className="flex align-items-center gap-2">
@@ -97,7 +102,7 @@ export default function Item() {
                     </div>
                     <div className="flex align-items-center justify-content-between">
                         <span className="text-2xl font-semibold">${product.price}</span>
-                        <Button icon="pi pi-shopping-cart" className="p-button-rounded"></Button>
+                        <Button icon="pi pi-shopping-cart" className="p-button-rounded" onClick={() => setProductVisible(product, true)}></Button>
                     </div>
                 </div>
             </div>
@@ -123,7 +128,76 @@ export default function Item() {
 
     return (
         <div className="card">
-            <DataView value={products} itemTemplate={itemTemplate} layout={layout} header={header()} />
+
+            <Dialog header="Details" visible={visible} modal={false} style={{ width: '25vw' }} onHide={() => setVisible(false)}>
+                <p className="m-4">
+
+                    <img src={`http://localhost:3000/coins/download/${selected_product.Photo1}`} alt={selected_product.Photo1} style={{ width: '50%', padding: '10px' }} />
+                    <img src={`http://localhost:3000/coins/download/${selected_product.Photo2}`} alt={selected_product.Photo2} style={{ width: '50%', padding: '10px' }} />
+                    <div>
+                            <Divider />
+                            
+                            <div className="text-orange-500 w-16rem h-1rem text-sm font-bold">
+                                Name: {selected_product.Name}
+                            </div>
+                            <Divider />
+                            <div className="text-orange-500 w-16rem h-1rem text-sm font-bold">
+                               Continent: {selected_product.Continent}
+                            </div>
+                            <Divider />
+                            <div className="text-orange-500 w-16rem h-1rem text-sm font-bold">
+                            Country: {selected_product.Country}
+                            </div>
+                            <Divider />
+                            <div className="text-orange-500 w-16rem h-1rem text-sm font-bold">
+                            Value: {selected_product.Value}
+                            </div>
+                            <Divider />
+                            <div className="text-orange-500 w-16rem h-1rem text-sm font-bold">
+                            Year: {selected_product.Year}
+                            </div>
+                            <Divider />
+                            <div className="text-orange-500 w-16rem h-1rem text-sm font-bold">
+                            Composition: {selected_product.Composition}
+                            </div>
+                            <Divider />
+                            <div className="text-orange-500 w-16rem h-1rem text-sm font-bold">
+                            Status: {selected_product.Status}
+                            </div>
+                            <Divider />
+                            <div className="text-orange-500 w-16rem h-1rem text-sm font-bold">
+                            References: {selected_product.References}
+                            </div>
+                            <Divider />
+                            <div className="text-orange-500 w-16rem h-1rem text-sm font-bold">
+                            Stock: {selected_product.Stock}
+                            </div>
+                            <Divider />
+                            {/* Price: {selected_product.Price}
+                            <Divider /> */}
+                    </div>
+            <div className="card flex flex-wrap gap-3 p-fluid">
+                <label className="font-bold block mb-2"></label>
+                <div className="flex-auto">
+                    <label htmlFor="mile" className="font-bold block mb-2"></label>
+                    <InputNumber value={ordered_qtty} onValueChange={(e) => setOrdered_qtty(e.value)} 
+                    mode="decimal" showButtons min={0} max={100} />
+                </div>
+                <div className="flex-auto">
+                <label className="font-bold block mb-2"></label>
+                <Button label="Add"></Button>    
+                </div>
+                <div className="flex-auto">
+                <label className="font-bold block mb-2"></label>
+                <Button label="Order"  ></Button> 
+                </div>   
+            </div>
+                </p>
+            </Dialog>
+
+            <DataView value={products} itemTemplate={itemTemplate} layout={layout}
+                // header={header()} --if we want buttons for list and grid
+                paginator rows={12} />
         </div>
     )
 }
