ffe99645ad214bde880fc668119d68cd7867a2e7
Razvan <razvan.mustata@gmail.com>
2023-11-30 13:00:31 +0200

    added ordered items

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -11,49 +11,125 @@ import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
+import { Divider } from 'primereact/divider';
+import { DataScroller } from 'primereact/datascroller';
+import { Rating } from 'primereact/rating';
+import { Tag } from 'primereact/tag';
+import { InputNumber } from 'primereact/inputnumber';
+
 
 export default function Order() {
-    const [retrievedObject, setRetrievedObject] = useState([{"Code": "UN"}]);
-    
+  const [retrievedObject, setRetrievedObject] = useState([{ "Code": "UN" }]);
+  const [newQtty, setNewQtty] = useState(0);
 
-    const router = useRouter()
+  const header = (
+    <img alt="Card" src="https://primefaces.org/cdn/primereact/images/usercard.png" />
 
-    useEffect(() => {
-        setRetrievedObject(JSON.parse(localStorage.getItem('YourOrder')))
-      }, [])
+  );
+  const footer = (
+    <>
+      <Button label="Edit" icon="pi pi-check" />
+      <Button label="Delete" severity="danger" icon="pi pi-times" style={{ marginLeft: '0.5em' }} />
+    </>
+  );
 
 
-    const OrderedItems = () => {
+  const router = useRouter()
 
-        return (
-            <div>
-                    <DataTable value={retrievedObject} tableStyle={{ minWidth: '50rem' }}>
-                        <Column field="Catalog" header="Catalog"></Column>
-                        <Column field="Code" header="Code"></Column>
-                        <Column field="Composition" header="Composition"></Column>
-                        <Column field="Country" header="Country"></Column>
-                        <Column field="Name" header="Name"></Column>
-                        <Column field="Price" header="Price"></Column>
-                        <Column field="Qtty" header="Qtty"></Column>
-                        <Column field="Status" header="Status"></Column>
-                        <Column field="Photo1" header="Photo1"></Column>
-                        <Column field="Photo2" header="Photo2"></Column>
-                    </DataTable>
-                    <Button label="Place Order" />
+
+
+  // const CardOrderedItems = () => {
+  //   return (
+  //  <div>
+  //               {retrievedObject.map(item => 
+  //               <Card key = {item.Code} title={item.Name} subTitle={item.Catalog} footer={footer}  className="md:w-55rem">
+  //                 <p className="m-0">
+  //                   {/* <img src={`http://localhost:3000/coins/download/${item.Photo1}`} 
+  //                     alt={item.Photo1} style={{ width: '5%', padding: '10px' }} />
+  //                     <img src={`http://localhost:3000/coins/download/${item.Photo2}`} 
+  //                     alt={item.Photo2} style={{ width: '5%', padding: '10px' }} />   */}
+  //                     {item.Catalog}
+  //                     <Divider />
+  //                     {item.Country}
+  //                     <Divider />
+  //                     {item.Code}
+  //                     <Divider />
+  //                     {item.Qtty}
+  //                     <Divider />
+  //                     {item.Price}
+  //                     <Divider />
+  //                     {item.Status}
+  //                     <Divider />
+  //                 </p>
+  //             </Card>)}
+  //             <Button label="Place Order" />
+  //       </div>
+  // )}
+
+  const itemTemplate = (data) => {
+    return (
+      <div className="col-12 border-1 border-blue-300">
+        <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+          <img src={`http://localhost:3000/coins/download/${data.Photo1}`} alt={data.Photo1} style={{ width: '8%', padding: '10px' }} />
+          <img src={`http://localhost:3000/coins/download/${data.Photo2}`} alt={data.Photo2} style={{ width: '8%', padding: '10px' }} />
+          <div className="flex flex-column lg:flex-row justify-content-between align-items-center xl:align-items-start lg:flex-1 gap-4">
+            <div className="flex flex-column align-items-center lg:align-items-start gap-3">
+              <div className="flex flex-column gap-1">
+                <div className="text-2xl font-bold text-900">Composition: {data.Name}</div>
+                <div className="text-2xl font-bold text-900"> Catalog: {data.Catalog}</div>
+                <div className="text-2xl font-bold text-900">Composition: {data.Composition}</div>
+                <div className="text-2xl font-bold text-900">Composition: {data.Country}</div>
+                <div className="text-2xl font-bold text-900">Composition: {data.Price}</div>
+                <div className="text-2xl font-bold text-900">Composition: {data.Status}</div>
+              </div>
+              <div className="flex flex-column gap-2">
+                <span className="flex align-items-center gap-2 text-2xl font-bold text-900">
+                  Qtty: <InputNumber inputId="minmax-buttons" value={data.Qtty} onValueChange={(e) => setNewQtty(e.value)} mode="decimal" showButtons min={0} max={1000} />
+                </span>
+              </div>
             </div>
-        
-        )
-    };
+            <div className="flex flex-row lg:flex-column align-items-center lg:align-items-end gap-4 lg:gap-2">
+              <span className="text-4xl font-semibold"> Value: {data.Price * data.Qtty}</span>
+
+              <div className="flex-auto">
+                <label htmlFor="minmax-buttons" className="font-bold block mb-2">Year</label>
+
+                {/* <InputNumber value={data.Qtty} onValueChange={(e) => setNewQtty(e.value)} showButtons buttonLayout="vertical" style={{ width: '4rem' }} 
+                    decrementButtonClassName="p-button-secondary" incrementButtonClassName="p-button-secondary" incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus" /> */}
+              </div>
+
+              <Tag value={data.Year}></Tag>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    setRetrievedObject(JSON.parse(localStorage.getItem('YourOrder')))
+  }, [])
+
+  const OrderedItems = () => {
+
+    return (
+      <div className="card ">
+        <DataScroller value={retrievedObject} itemTemplate={itemTemplate} rows={5} buffer={0.4} header="List of ordered items" />
+      </div>
+
+
+    )
+  };
 
   return (
     <PrimeReactProvider>
-
       <Menu activatedIndex={0} />
       <div>
-          Order page
-    <OrderedItems />
-  
-</div>
+
+        <OrderedItems />
+
+      </div>
     </PrimeReactProvider>
 
   )
--- components/menu.tsx
+++ components/menu.tsx
@@ -8,10 +8,15 @@ import React, { useState, useEffect } from 'react';
 import { TabMenu } from 'primereact/tabmenu';
 import { useRouter } from 'next/navigation'
 import '../css/style.css';
+import { Badge } from 'primereact/badge';
+import { Button } from 'primereact/button';
+import { Menubar } from 'primereact/menubar';
 
 export default function Menu(props) {
   const router = useRouter();
   const [activeMenuIndex, setMenuIndex] = useState(props.activatedIndex);
+  const [itemCount, setItemCount] = useState(0);
+
   const items = [
     { label: 'Home', icon: 'pi pi-fw pi-home' },
     { label: 'News', icon: 'pi pi-fw pi-bitcoin' },
@@ -21,46 +26,86 @@ export default function Menu(props) {
     { label: 'Admin', icon: 'pi pi-fw pi-cog' },
   ];
 
-// setMenuIndex(activeIndex); 
- return (
+  const checkOrderedItem = () => {
+
+    var existingData = localStorage.getItem("YourOrder");
+
+    // Parse existing data (if any)
+    var existingArray = existingData ? JSON.parse(existingData) : [];
+
+    const itemCount = existingArray.length;
+    setItemCount(itemCount);
+
+  }
+
+ const goToOrder = () => {
+  router.push('/order');
+ }
+
+  useEffect(() => {
+    checkOrderedItem()
+}, [])
+
+// const start = <Avatar alt="logo" image="./logo.png" size="xlarge" shape="circle" height="30" className="mr-2"/>
+// const start = <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="40" className="mr-2"></img>;
+// const end = (        
+// <i className="pi pi-shopping-cart p-overlay-badge" style={{ fontSize: '2rem' }}>
+// <Badge value={itemCount} severity="danger"></Badge>
+// </i>);
+
+
+  // setMenuIndex(activeIndex); 
+  return (
     <PrimeReactProvider >
 
       <div className="sticky min-w-full border-round bg-white font-bold flex align-items-start "
       // className="sticky"
       >
-        <TabMenu 
-        className ="text-3xl w-12"
-        model={items} activeIndex={activeMenuIndex} 
-        onTabChange={(e) => {
-          setMenuIndex(activeMenuIndex)
-          switch(e.index){
-            case 5: 
-              router.push('/admin');
-              
-              break;
-            case 4: 
-              router.push('/contact');
-              
-              break;
-            case 3: 
-              router.push('/conditions');
-             
-              break;
-            case 2: 
-              router.push('/lots');
-              
-              break;
-            case 1: 
-              router.push('/news');
-              
-              break;
-            case 0: 
-              router.push('/');
-              
-              break;           
+            {/* <div className="card">
+            <Menubar model={items} start={start} end={end} />
+        </div> */}
+
+        <TabMenu
+          className="text-2xl w-12"
+          model={items} activeIndex={activeMenuIndex}
+          onTabChange={(e) => {
+            setMenuIndex(activeMenuIndex)
+            switch (e.index) {
+              case 5:
+                router.push('/admin');
+
+                break;
+              case 4:
+                router.push('/contact');
+
+                break;
+              case 3:
+                router.push('/conditions');
+
+                break;
+              case 2:
+                router.push('/lots');
+
+                break;
+              case 1:
+                router.push('/news');
+
+                break;
+              case 0:
+                router.push('/');
+
+                break;
+            }
           }
-        }
           } />
+        
+        {/* <Button  className='border-round font-bold flex' text icon="pi pi-shopping-cart" outlined badge={itemCount} badgeClassName="p-badge-danger" size="large" onClick={goToOrder}/> */}
+       
+       <a href="/order">
+        <i className="pi pi-shopping-cart p-overlay-badge" style={{ fontSize: '2rem' }}>
+                <Badge value={itemCount} severity="danger"></Badge>
+        </i>
+        </a>
       </div>
     </PrimeReactProvider>
   )
